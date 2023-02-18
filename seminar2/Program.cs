5e1c7f67830d9ed45f7e47e1743290a223e280b6
Anton <toshagudi@yandex.ru>
2023-02-18 14:05:35 +0300

    семинар 2

---
+++ seminar2/Program.cs
@@ -0,0 +1,24 @@
+﻿int i = 1;
+while (i <= 36)
+{
+    if (i % 3 == 0 && i % 6 == 0)
+    {
+        System.Console.WriteLine("FizzBuzz");
+    }     
+    else if(i % 3 == 0)
+    {
+        System.Console.WriteLine("Fizz");
+    }
+    else if(i % 5 == 0)
+    {
+        System.Console.WriteLine("Buzz");
+    }
+    
+    else
+    {
+        System.Console.WriteLine(i);
+    }
+
+    i++;
+
+}
