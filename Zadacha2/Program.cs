4441e55cfd69e9459b3c3b26a80b66fbc6ad0d34
Anton <toshagudi@yandex.ru>
2023-02-18 14:14:52 +0300

    задача2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿{
+    int a, b, c;
+    Console.WriteLine("Введите три числа: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    c = Convert.ToInt32(Console.ReadLine());
+    int max = a; 
+    if (b > max)
+    { 
+    max = b;
+    }
+   
+    if (c > max) 
+    {
+    max = c;
+    }
+   
+    Console.WriteLine("Наибольшее из трех: " + max);
+}   
