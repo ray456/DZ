ecae83af4e590485a045878c0aacd603156d6a4c
Anton <toshagudi@yandex.ru>
2023-02-18 14:07:01 +0300

    задача 19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int reversedNumber = 0;
+        int temp = number;
+        while (temp > 0)
+        {
+            reversedNumber = reversedNumber * 10 + temp % 10;
+            temp /= 10;
+        }
+        if (number == reversedNumber)
+            Console.WriteLine("Число палиндромное.");
+        else
+            Console.WriteLine("Число не палиндромное.");
