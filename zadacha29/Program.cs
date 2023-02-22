67f9194b507fe94559891b5b8a335a0576019a48
Anton <toshagudi@yandex.ru>
2023-02-22 12:52:03 +0300

    реш. зад. 29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,25 @@
+﻿
+void FillArray(int[] array)
+{
+    int length = array.Length;
+
+    for (int index = 0; index < length; index++)
+    {
+        array[index] = new Random().Next(0, length);
+        Console.Write(array[index] + " ");
+    }
+}
+
+void PrintArray(int[] mas)
+{
+    int len = mas.Length;
+    for (int i = 0; i < len; i++)
+    {        
+        Console.Write(mas[i] + " ");
+    }
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+Console.WriteLine();
+PrintArray(arr);
