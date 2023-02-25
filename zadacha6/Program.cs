6044ee88e78323abbafabbdc56935c9f76e26953
Anton <toshagudi@yandex.ru>
2023-02-25 20:50:27 +0300

    решение.зад.6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int x = 0;
+x = Convert.ToInt32(Console.ReadLine());
+
+if ((x % 2) == 0)
+{
+    Console.WriteLine("Число четное " );
+}
+else
+{
+    Console.WriteLine("Число нечетное ");
+}
+
