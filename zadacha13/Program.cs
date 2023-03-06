8f817a1708d046fb77cd38cc951f988af54e91a4
Anton <toshagudi@yandex.ru>
2023-03-06 22:31:57 +0300

    решение задачи13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число > ");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+
+{
+    if (number >= 100)
+    {
+        while (number > 999)
+        number = number / 10; 
+        x = number % 10;
+    }
+    
+Console.WriteLine("третья цифра " + x);
+}  
+{  
+     if (number < 100)
+    Console.WriteLine("число не трехзначное ");
+}
