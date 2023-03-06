e47c1cc938c13f328a8b5e31c801720bb28c4096
Anton <toshagudi@yandex.ru>
2023-03-06 22:27:55 +0300

    решение

---
+++ zadacha15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число > ");
+int number = Convert.ToInt32(Console.ReadLine());
+// if (number > 7)
+// {
+//     Console.WriteLine("Это не день недели");
+// }
+if (number > 5 && number <= 7)
+{
+   
+    Console.WriteLine("Выходной день");
+
+}
+else 
+{
+    Console.WriteLine("Рабочий день");
+}
