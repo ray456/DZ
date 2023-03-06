54b0e0f330e3e783b13e80063e9d5a285135e0f5
Anton <toshagudi@yandex.ru>
2023-03-06 22:15:24 +0300

    изм.решения

--- zadacha10/Program.cs
+++ zadacha10/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 Console.WriteLine("Введите трехзначное число > ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Введенное число {number} ");
+
 int secondRank = number / 10 % 10;
+
 Console.WriteLine($"Вторая цифра {secondRank} ");
