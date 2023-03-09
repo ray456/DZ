280745770670e0d639e18b7a9c7251c307f82691
Anton <toshagudi@yandex.ru>
2023-03-09 20:24:55 +0300

    решение задачи27

---
+++ zadacha27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число > ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+    Console.WriteLine($"Сумма всех чисел в цифре  = {sum}");
+}
+
