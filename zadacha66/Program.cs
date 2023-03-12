90b355f89714040eaca92519bc3e3fc8944739d6
Anton <toshagudi@yandex.ru>
2023-03-12 21:39:28 +0300

    решение задачи 66

---
+++ zadacha66/Program.cs
@@ -0,0 +1,14 @@
+﻿int M = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine()); ;
+NaturalNumbers(N, M);
+
+void NaturalNumbers(int m, int n)
+{
+    
+    if(m < n)  return;
+    {
+        Console.Write($"{n} ");
+        NaturalNumbers(m, n + 1);
+    }
+}
+
