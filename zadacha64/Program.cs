4b354cf1eb04fc8c3f0a878b236bd432fb552067
Anton <toshagudi@yandex.ru>
2023-03-12 20:51:11 +0300

    решение 64 задачи

---
+++ zadacha64/Program.cs
@@ -0,0 +1,17 @@
+﻿int N = int.Parse(Console.ReadLine());
+NaturalNumbers(N);
+
+for(int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    Console.Write($"{i} ");
+}
+void NaturalNumbers(int n)
+{
+    if(n != 0) return;
+    else 
+    {
+        Console.Write($"{n} ");
+        NaturalNumbers(n-1);
+    }
+}
