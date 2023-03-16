880ef3465a8ed1b7f9f216dfb93ddcd680ad20f9
Anton <toshagudi@yandex.ru>
2023-03-16 22:31:06 +0300

    реш.задачи 68

---
+++ zadacha68/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int m, int n)
+{
+    if(m == 0) return n+1;
+    else if(n == 0) return Akkerman(m-1, 1);
+    else return Akkerman(m-1, Akkerman(m, n-1));
+}
+Console.Write($"Функция Аккермана равна {Akkerman(m, n)}");
